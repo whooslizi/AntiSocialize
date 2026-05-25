40f04c74a73231dc6469bb5b6db0b1124f760e9e
Lizi <86664792+whooslizi@users.noreply.github.com>
2026-05-25 15:22:39 +0700

    fix:  Changed the default isBlocking state on installation from true to false. This prevents the popup buttons from being frozen/locked out immediately after installing the extension

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.get(['isBlocking', 'blockedSites'], (data) => {
     if (data.isBlocking === undefined) {
       chrome.storage.local.set({ 
-        isBlocking: true, 
+        isBlocking: false, 
         blockingMode: 'infinite',
         blockedSites: ['facebook.com', 'tiktok.com', 'instagram.com', 'youtube.com', 'reddit.com'],
         startTime: Date.now(),
